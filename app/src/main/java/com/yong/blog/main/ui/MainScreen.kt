f2c8c97d2a7dfbb6245b2a95ff5628eabde829ff
yymin1022 <yymin1022@gmail.com>
2025-06-16 23:52:17 +0900

    Update Main UI : Applied logging for main

--- app/src/main/java/com/yong/blog/main/ui/MainScreen.kt
+++ app/src/main/java/com/yong/blog/main/ui/MainScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -15,6 +16,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.yong.blog.R
+import com.yong.blog.common.util.FirebaseUtil
 
 @Composable
 fun MainScreen(
@@ -22,6 +24,10 @@ fun MainScreen(
     navigateToList: (String) -> Unit,
     navigateToDetail: (String, String) -> Unit,
 ) {
+    LaunchedEffect(Unit) {
+        FirebaseUtil.logMainEvent()
+    }
+
     Scaffold(
         modifier = modifier,
     ) { innerPadding ->
