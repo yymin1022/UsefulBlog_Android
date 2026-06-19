04408155695abe88aad9556a5b8acb64eabf184f
yymin1022 <yymin1022@gmail.com>
2026-06-19 17:43:15 +0900

    Update Project : Update AndroidX / Compose / Kotlin dependencies as latest version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.core)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
